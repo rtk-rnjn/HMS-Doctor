5ceecb29d2f4f4357c1e70a1ac99b7e4fcddd872
Harshchauhan177 <harsh7617rajput@gmail.com>
2025-03-24 18:15:59 +0530

    eyeButton properly function

--- HMS-Doctor/Controllers/SignInViewController.swift
+++ HMS-Doctor/Controllers/SignInViewController.swift
@@ -14,10 +14,59 @@ class SignInViewController: UIViewController {
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        navigationItem.hidesBackButton = true
-    }
+    let eyeButton = UIButton(type: .custom)
+        
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            eyeButton.tintColor = .gray
+            eyeButton.isEnabled = false
+            configureEyeButton(for: passwordTextField)
+            passwordTextField.addTarget(self, action: #selector(passwordEntered), for: .editingChanged)
+            
+            
+            
+            
+            
+            navigationItem.hidesBackButton = true
+        }
+
+    
+    @objc func passwordEntered(sender:UITextField){
+            if passwordTextField.text?.isEmpty ?? true || passwordTextField.text == "" {
+                eyeButton.isEnabled = false
+                eyeButton.tintColor = .gray
+            }else{
+                eyeButton.isEnabled = true
+                eyeButton.tintColor = .tintColor
+            }
+        }
+        
+        private func configureEyeButton(for textField: UITextField) {
+            // Create the eye button
+            eyeButton.setImage(UIImage(systemName: "eye"), for: .normal)
+            eyeButton.setImage(UIImage(systemName: "eye.slash"), for: .selected)
+            eyeButton.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+            eyeButton.addTarget(self, action: #selector(togglePasswordVisibility(_:)), for: .touchUpInside)
+
+            // Create a container view to add padding
+            let containerView = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: 30))
+            containerView.addSubview(eyeButton)
+
+            // Adjust the button's position within the container view
+            eyeButton.frame = CGRect(x: -8, y: 0, width: 30, height: 30) // Adds a 10-point margin on the right
+
+            // Set the container view as the right view of the text field
+            textField.rightView = containerView
+            textField.rightViewMode = .always
+            textField.isSecureTextEntry = true // Ensure secure entry initially
+        }
+        
+        @objc private func togglePasswordVisibility(_ sender: UIButton) {
+            sender.isSelected.toggle()
+            passwordTextField.isSecureTextEntry.toggle()
+        }
+
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "segueShowResetPasswordViewController", let resetPasswordViewController = segue.destination as? ResetPasswordViewController {
