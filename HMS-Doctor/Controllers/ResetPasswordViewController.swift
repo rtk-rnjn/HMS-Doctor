d32a18c9e3477faaf15fc3fdc61bd7cdb48dfa1b
Harshchauhan177 <harsh7617rajput@gmail.com>
2025-03-24 19:41:35 +0530

    eyeButton proprly function in reset password

--- HMS-Doctor/Controllers/ResetPasswordViewController.swift
+++ HMS-Doctor/Controllers/ResetPasswordViewController.swift
@@ -13,10 +13,86 @@ class ResetPasswordViewController: UIViewController {
     @IBOutlet var passwordField: UITextField!
     @IBOutlet var confirmPasswordField: UITextField!
 
+    
+    let eyeButton1 = UIButton(type: .custom)
+    let eyeButton2 = UIButton(type: .custom)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+//        admin = DataController.shared.admin
+        eyeButton1.tintColor = .gray
+        eyeButton2.tintColor = .gray
+        eyeButton1.isEnabled = false
+        eyeButton2.isEnabled = false
+        eyeButton1.tag = 10
+        configureEyeButton(for: passwordField,button: eyeButton1)
+        configureEyeButton(for: confirmPasswordField,button: eyeButton2)
+        passwordField.addTarget(self, action: #selector(passwordEntered), for: .editingChanged)
+        confirmPasswordField.addTarget(self, action: #selector(passwordEnteredForCnfrmPass), for: .editingChanged)
+        
+        
         staff = DataController.shared.staff
     }
+    
+    
+    @objc func passwordEntered(sender:UITextField){
+        if passwordField.text?.isEmpty ?? true || passwordField.text == "" {
+            eyeButton1.isEnabled = false
+            eyeButton1.tintColor = .gray
+        }else{
+            eyeButton1.isEnabled = true
+            eyeButton1.tintColor = .tintColor
+        }
+    }
+    
+    @objc func passwordEnteredForCnfrmPass(sender:UITextField){
+        if confirmPasswordField.text?.isEmpty ?? true || confirmPasswordField.text == "" {
+            eyeButton2.isEnabled = false
+            eyeButton2.tintColor = .gray
+        }else{
+            eyeButton2.isEnabled = true
+            eyeButton2.tintColor = .tintColor
+        }
+    }
+    
+    private func configureEyeButton(for textField: UITextField,button:UIButton) {
+        // Create the eye button
+        button.setImage(UIImage(systemName: "eye"), for: .normal)
+        button.setImage(UIImage(systemName: "eye.slash"), for: .selected)
+        button.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        button.addTarget(self, action: #selector(togglePasswordVisibility(_:)), for: .touchUpInside)
+
+        // Create a container view to add padding
+        let containerView = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: 30))
+        containerView.addSubview(button)
+
+        // Adjust the button's position within the container view
+        button.frame = CGRect(x: -8, y: 0, width: 30, height: 30) // Adds a 10-point margin on the right
+
+        // Set the container view as the right view of the text field
+        textField.rightView = containerView
+        textField.rightViewMode = .always
+        textField.isSecureTextEntry = true // Ensure secure entry initially
+    }
+    
+    @objc private func togglePasswordVisibility(_ sender: UIButton) {
+        sender.isSelected.toggle()
+        if sender.tag == 10{
+            passwordField.isSecureTextEntry.toggle()
+        }
+        else{
+            confirmPasswordField.isSecureTextEntry.toggle()
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 
     @IBAction func signInButtonTapped(_ sender: UIButton) {
         guard let password = passwordField.text, !password.isEmpty else {
