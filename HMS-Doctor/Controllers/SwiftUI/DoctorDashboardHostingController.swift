447b0755f9757140e23a6b5442db9a181a5bfb9d
Ritik Ranjan <ritik0ranjan+github@gmail.com>
2025-03-26 17:25:58 +0530

    Minor code improvements

--- HMS-Doctor/Controllers/SwiftUI/DoctorDashboardHostingController.swift
+++ HMS-Doctor/Controllers/SwiftUI/DoctorDashboardHostingController.swift
@@ -1,22 +1,8 @@
 import SwiftUI
 
 class DoctorDashboardHostingController: UIHostingController<DoctorDashboardView> {
-
-    // MARK: Lifecycle
-
     required init?(coder: NSCoder) {
         let swiftUIView = DoctorDashboardView()
         super.init(coder: coder, rootView: swiftUIView)
-    }
-
-    // MARK: Internal
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        navigationItem.title = "Dashboard"
-
-        // Enable large titles
-        navigationController?.navigationBar.prefersLargeTitles = true
-        navigationItem.largeTitleDisplayMode = .always
     }
 }
